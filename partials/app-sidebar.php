34dc8f4cfdcf0fb44351c122ffd421ede864a7be
JNGS <143780288+jiffgals@users.noreply.github.com>
2024-02-01 22:01:52 +0800

    Add files via upload

---
+++ partials/app-sidebar.php
@@ -0,0 +1,157 @@
+<?php
+    $user = $_SESSION['$user']
+?>
+
+<div class="dashboard_sidebar" id="dashboard_sidebar">
+    <h3 class="dashboard_logo" id="dashboard_logo">Admin</h3>
+        <div class="dashboard_sidebar_user">
+            <img src="images/user/userimage.jpg" alt="user image" id="userImage" />
+            <span><?= $user['first_name'] . ' ' . $user['last_name'] ?></span>
+        </div>
+        
+        <div class="dropdownS">
+            <button class="dropbtnS">Search</button>
+            <div class="dropdown-contentS">
+                <a href="stock_search.php">Product/Items</a>
+                <a href="supplier_search.php">Supplier Details</a>
+                <a href="sales_search.php">Sales Data</a>
+            </div>
+        </div>
+        
+        <div class="dashboard_sidebar_menus">
+            <ul class="dashboard_menu_lists">
+                <!--  class="menuActive" -->
+                <li class="liMainMenu">
+                    <a href="./dashboard.php">
+                    <svg width="20px" xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path fill="#fff" 
+                        d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm320 96c0-26.9-16.5-49.9-40-59.3V88c0-13.3-10.7-24-24-24s-24 10.7-24 24V292.7c-23.5 9.5-40 32.5-40 59.3c0 35.3 28.7 64 64 64s64-28.7 64-64zM144 176a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm-16 80a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM400 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/>
+                    </svg> &nbsp; <span class="menuText">Dashboard</span></a>
+                </li>
+
+                <li class="liMainMenu">
+                    <a href="./report.php">
+                    <svg width="20px" xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path fill="#fff" 
+                        d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"/>
+                    </svg> &nbsp; <span class="menuText">Reports</span></a>
+                </li>
+
+                <li class="liMainMenu">
+                    <a href="javascript:void(0);" class="showHideSubMenu">
+                        <svg class="showHideSubMenu" width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fff" d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg> &nbsp;
+                        <!-- <i class="fa fa-tag showHideSubMenu"></i> -->
+                        <span class="menuText showHideSubMenu">Products</span>
+                        <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                        
+                    </a>
+
+                    <ul class="subMenus">
+                        <li><a class="subMenuLink" href="./product-view.php"> 
+                            <svg width="14px" xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path fill="#f1922f" 
+                                d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg> 
+                                &nbsp; <span class="menuText">View Products</span>
+                            </a>
+                        </li>
+                        <li><a class="subMenuLink" href="./product-add.php"> 
+                            <svg width="13px" xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path fill="#f1922f" 
+                                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                                &nbsp; <span class="menuText">Add Products</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="liMainMenu">
+                    <a href="javascript:void(0);" class="showHideSubMenu">
+                        <svg class="showHideSubMenu" width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fff" d="M256 48c0-26.5 21.5-48 48-48H592c26.5 0 48 21.5 48 48V464c0 26.5-21.5 48-48 48H381.3c1.8-5 2.7-10.4 2.7-16V253.3c18.6-6.6 32-24.4 32-45.3V176c0-26.5-21.5-48-48-48H256V48zM571.3 347.3c6.2-6.2 6.2-16.4 0-22.6l-64-64c-6.2-6.2-16.4-6.2-22.6 0l-64 64c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0L480 310.6V432c0 8.8 7.2 16 16 16s16-7.2 16-16V310.6l36.7 36.7c6.2 6.2 16.4 6.2 22.6 0zM0 176c0-8.8 7.2-16 16-16H368c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H16c-8.8 0-16-7.2-16-16V176zm352 80V480c0 17.7-14.3 32-32 32H64c-17.7 0-32-14.3-32-32V256H352zM144 320c-8.8 0-16 7.2-16 16s7.2 16 16 16h96c8.8 0 16-7.2 16-16s-7.2-16-16-16H144z"/></svg> &nbsp; 
+                        <!-- <i class="fa fa-shopping-cart showHideSubMenu"></i>  -->
+                        <span class="menuText showHideSubMenu">Stocks / Supplies</span>
+                        <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                    </a>
+
+                    <ul class="subMenus">
+                        <li><a class="subMenuLink" href="./view-order.php"> 
+                            <svg width="14px" xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path fill="#f1922f" 
+                                d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg> 
+                                &nbsp;  <span class="menuText">View Stocks / Supplies</span>
+                            </a>
+                        </li>
+                        <li><a class="subMenuLink" href="./product-order.php"> 
+                            <svg width="13px" xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path fill="#f1922f" 
+                                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                                &nbsp; <span class="menuText">Add Stocks / Supplies</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="liMainMenu">
+                    <a href="javascript:void(0);" class="showHideSubMenu">
+                        <svg class="showHideSubMenu" width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fff" d="M0 48C0 21.5 21.5 0 48 0H368c26.5 0 48 21.5 48 48V96h50.7c17 0 33.3 6.7 45.3 18.7L589.3 192c12 12 18.7 28.3 18.7 45.3V256v32 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H576c0 53-43 96-96 96s-96-43-96-96H256c0 53-43 96-96 96s-96-43-96-96H48c-26.5 0-48-21.5-48-48V48zM416 256H544V237.3L466.7 160H416v96zM160 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm368-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM176 80v48l-48 0c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h48v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V192h48c8.8 0 16-7.2 16-16V144c0-8.8-7.2-16-16-16H240V80c0-8.8-7.2-16-16-16H192c-8.8 0-16 7.2-16 16z"/></svg> &nbsp;
+                        <!-- <i class="fa fa-truck showHideSubMenu"></i>  -->
+                        <span class="menuText showHideSubMenu">Brand / Supplier</span>
+                        <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                    </a>
+
+                    <ul class="subMenus">
+                        <li><a class="subMenuLink" href="./supplier-view.php"> 
+                            <svg width="14px" xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path fill="#f1922f" 
+                                d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg> 
+                                &nbsp; <span class="menuText">View Brand / Supp</span>
+                            </a>
+                        </li>
+                        <li><a class="subMenuLink" href="./supplier-add.php"> 
+                            <svg width="13px" xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path fill="#f1922f" 
+                                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                                &nbsp; <span class="menuText">Add Brand / Supp</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="liMainMenu">
+                    <a href="javascript:void(0);" class="showHideSubMenu">
+                        <svg class="showHideSubMenu" width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fff" d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM504 312V248H440c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V136c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H552v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg> &nbsp;
+                        <!-- <i class="fa fa-user-plus showHideSubMenu"></i>  -->
+                        <span class="menuText showHideSubMenu">Users / Workers</span>
+                        <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                    </a>
+
+                    <ul class="subMenus">
+                        <li><a class="subMenuLink" href="./users-view.php"> 
+                            <svg width="14px" xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path fill="#f1922f" 
+                                d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg> 
+                                &nbsp; <span class="menuText">View User / Workers</span>
+                            </a>
+                        </li>
+                        <li><a class="subMenuLink" href="./users-add.php"> 
+                            <svg width="13px" xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                <path fill="#f1922f" 
+                                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                                &nbsp; <span class="menuText">Add User / Worker</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+        <a class="jngs" href="https://www.youtube.com/channel/UC8I9Lr6jqcLcb1z79fdkQKQ">Powered By: JNGS</a>
+            </ul>
+        </div>
+</div>
